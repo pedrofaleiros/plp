89d52a918a9f1e1cdb7036cec3b050adea870312
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-05-18 08:50:55 -0300

    ex11

---
+++ ex11/README.txt
@@ -0,0 +1,12 @@
+O programa estava levando muito tempo para
+apresentar todos os primos entre 0 e 99999
+Entao dei a opcao de apresentar todos os primos 
+entre 0 e 9999(em vez de 99999), com uma thread 
+para cada faixa de 1000 valores(em vez de 10000)
+
+Para rodar no terminal:
+python threads.py
+
+E depois escolher uma opcao:
+Aperte 2 para apresentar todos primos entre 0 e 99999
+e qualquer caractere para apresentar todos os primos entre 0 e 9999
---
+++ ex11/threads.py
@@ -0,0 +1,50 @@
+import threading
+
+def isPrime(num):
+	if num == 1 or num <= 0:
+		return False
+
+	cont = 0
+	for i in range(1, num+1):
+		if num % i == 0:
+			cont += 1
+
+	return cont == 2
+
+def prime(start, stop):
+
+	for i in range(start, stop):
+		if isPrime(i):
+			print(i)
+
+
+print("""
+	Leva muito tempo para apresentar
+	todos os primos entre 0 e 99999
+	Por isso fiz 2 opcoes:
+
+Opcao 1:
+	Apresentar numeros primos
+	entre 0 e 9999,
+	com uma thread para cada
+	faixa de 1000 valores
+	(Aperte qualquer caractere)
+
+Opcao 2 (Opcao do exercicio): 
+	Apresentar numeros primos 
+	entre 0 ate 99999 
+	(Aperte '2')
+""")
+res = input('Opcao: ')
+
+NUM_MAX = 1000
+if res == '2':
+	NUM_MAX = 10000
+
+nums = list(map(lambda x: x*NUM_MAX, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+print(nums)
+for i in range(0, 10):
+	inicio = nums[i]
+	fim = nums[i+1]
+	threading.Thread(target=prime, args=(inicio, fim)).start()
